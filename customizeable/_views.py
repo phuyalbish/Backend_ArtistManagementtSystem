08ec53e4c6876ca5400ba2ccf6a28d5f1ec33d92
phuyalbish <phuyalbish@gmail.com>
2024-06-10 21:29:19 +0545

    Customizable backend Complete

--- customizeable/_views.py
+++ customizeable/_views.py
@@ -1,23 +1,20 @@
 from rest_framework import status
 from rest_framework.views import APIView
-from user.models import Users
 from rest_framework.response import Response
-from customizeable.models import CustomTheme, CustomThemeSerializer
+from customizeable.models import CustomTheme
 from rest_framework.permissions import IsAuthenticated
 from core.permissions import IsArtist
 
 class SelectTheme(APIView):
     permission_classes = [IsAuthenticated & IsArtist]
-
     def patch(self, request, themeid):
         user = request.user
-        
         try:
             theme = CustomTheme.objects.get(id=themeid)
         except CustomTheme.DoesNotExist:
             return Response({'error': 'Theme not found'}, status=status.HTTP_404_NOT_FOUND)
-        
-        user.selected_theme = theme
+
+        user.theme = theme
         user.save()
 
         return Response({'message': 'Theme selected successfully'}, status=status.HTTP_200_OK)
--- customizeable/models.py
+++ customizeable/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from user.models import Users
 from rest_framework import serializers
 
 
