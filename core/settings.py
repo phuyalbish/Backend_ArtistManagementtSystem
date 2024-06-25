e66b62224af140f811879e18cc0cd1fb2a592835
Anuska-Shahi <anuskashahi11@gmail.com>
2024-06-25 16:05:56 +0545

    Revert "add db in env"
    This reverts commit dfd5bebb28006a27eb75290ef28b41bdf4817279.

--- core/settings.py
+++ core/settings.py
@@ -125,12 +125,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': os.getenv('DB_ENGINE'),
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'artist_management',
+        'USER': 'my_user',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',  # or your PostgreSQL host
+        'PORT': '5432',       # default PostgreSQL port
     }
 }
 # Password validation
