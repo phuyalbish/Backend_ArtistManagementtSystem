f17d4d173c56a7e26bda1b3b95efcbb8a0d0df13
phuyalbish <phuyalbish@gmail.com>
2024-06-26 16:05:51 +0545

    Dockerized

--- core/settings.py
+++ core/settings.py
@@ -194,7 +194,7 @@ REST_FRAMEWORK = {
 
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=3),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=90),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': False,
